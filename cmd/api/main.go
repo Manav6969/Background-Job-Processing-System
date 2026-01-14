53b7201b66ab59a1fa73a8598c38c4c3b19469d6
Manav Agarwal <manavagarwal578@gmail.com>
2026-01-14 14:03:28 +0530

    persist jobs in PostgreSQL with pending status

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-    "github.com/Manav6969/Background-Job-Processing-System/internal/config"
-    "github.com/Manav6969/Background-Job-Processing-System/internal/server"
+	"github.com/Manav6969/Background-Job-Processing-System/internal/config"
+	"github.com/Manav6969/Background-Job-Processing-System/internal/db"
+	"github.com/Manav6969/Background-Job-Processing-System/internal/server"
 )
 
 func main() {
-    cfg := config.Load()
-    server.Run(cfg)
+	cfg := config.Load()
+	err := db.Connect("postgres://postgres@localhost:5432/backgroundjobprocessingsystem")
+	if err != nil {
+		panic(err)
+	}
+	server.Run(cfg)
 }
--- internal/job/handler.go
+++ internal/job/handler.go
@@ -1,14 +1,16 @@
 package job
 
 import (
+	"context"
 	"encoding/json"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/Manav6969/Background-Job-Processing-System/internal/db"
 	"github.com/Manav6969/Background-Job-Processing-System/internal/queue"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Job struct {
+	ID      int         `json:"id"`
 	Type    string      `json:"type"`
 	Payload interface{} `json:"payload"`
 }
@@ -22,6 +24,23 @@ func Create(c *gin.Context) {
 		return
 	}
 
+	var jobID int
+
+	err := db.Conn.QueryRow(
+		context.Background(),
+		"INSERT INTO jobs(type, payload, status) VALUES($1, $2, $3) RETURNING id",
+		job.Type,
+		job.Payload,
+		"pending",
+	).Scan(&jobID)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "db error"})
+		return
+	}
+
+	job.ID = jobID
+
 	data, _ := json.Marshal(job)
 	_ = q.Push(string(data))
 
@@ -30,8 +49,7 @@ func Create(c *gin.Context) {
 	})
 }
 
-
 func Get(c *gin.Context) {
-    id := c.Param("id")
-    c.JSON(http.StatusOK, gin.H{"id": id, "status": "pending"})
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"id": id, "status": "pending"})
 }
