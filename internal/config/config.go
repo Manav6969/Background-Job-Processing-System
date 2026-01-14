d350293c8f9123c160ea24f8aab33aea0e822669
Manav Agarwal <manavagarwal578@gmail.com>
2026-01-14 19:59:01 +0530

    Added jwt in env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,29 @@
 package config
 
+import (
+	"os"
+)
 
 type Config struct {
 	JWTSecret string
 	Port      string
 	RateLimit int
-	
 }
 
 func Load() *Config {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		panic("JWT_SECRET is not set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":6969"
+	}
+
 	return &Config{
-		JWTSecret: "dev_secret_change_later",
-		Port:      ":8080",
+		JWTSecret: jwtSecret,
+		Port:      port,
 		RateLimit: 5,
 	}
 }
